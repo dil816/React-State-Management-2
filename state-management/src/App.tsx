5f50453497453874e1aef464dabfe7fd1e4505c9
dil816 <Rivindudilochana18329@gmail.com>
2024-07-06 09:36:38 +0530

    native context

--- state-management/src/App.tsx
+++ state-management/src/App.tsx
@@ -1,7 +1,64 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+interface Pokemon {
+  id: number;
+  name: string;
+  type: string[];
+  hp: number;
+  attack: number;
+  defence: number;
+  special_attack: number;
+  special_defence: number;
+  speed: number;
+}
+
+function usePokemonSource(): {
+  pokemon: Pokemon[];
+} {
+  const [pokemon, setPokemon] = useState<Pokemon[]>([]);
+
+  useEffect(() => {
+    fetch("/pokemon.json")
+      .then((response) => response.json())
+      .then((data) => setPokemon(data));
+  }, []);
+
+  return { pokemon };
+}
+
+/*
+  const PokemonContext = createContext({
+  pokemon: [] as Pokemon[],
+ });
+
+ */
+
+const PokemonContext = createContext<ReturnType<typeof usePokemonSource>>(
+  {} as unknown as ReturnType<typeof usePokemonSource>
+);
+
+function usePokemon() {
+  return useContext(PokemonContext);
+}
+
+const PokemonList = () => {
+  const { pokemon } = usePokemon();
+  return (
+    <div>
+      {pokemon.map((p) => (
+        <div key={p.id}>{p.name}</div>
+      ))}
+    </div>
+  );
+};
+
 function App() {
+  //const { pokemon } = usePokemon();
   return (
     <>
-      <div></div>
+      <PokemonContext.Provider value={usePokemonSource()}>
+        <PokemonList />
+      </PokemonContext.Provider>
     </>
   );
 }
